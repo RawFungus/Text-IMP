f5d6d5e0cc1c19f0fe2ff577fa3a36a11b56244f
RawFungus <104890353+RawFungus@users.noreply.github.com>
2025-03-24 14:58:33 +0300

    Update Text_IMP_v1.18.5.jsx
    Fixed an incorrect license signature 😶‍🌫️

--- Text_IMP_v1.18.5.jsx
+++ Text_IMP_v1.18.5.jsx
@@ -2,10 +2,19 @@
 // Требования: Legacy ExtendScript (JavaScript) для Adobe After Effects  
 // Автор: RawFungus  (https://github.com/RawFungus)
 
-// Copyright(c) 2025 Evgeny G.
-// https://github.com/RawFungus
-// This script is distributed under the MIT License.
-// See the LICENSE file for details.
+// Copyright (C) 2025 Evgeny G.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 
 // --- Основная функция для создания пользовательского интерфейса ---
